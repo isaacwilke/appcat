9316862b131c6ad63022a6924cc3e6914cb9a491
vaibhavbhasin <vaibhav.bhasin@yahoo.co.in>
2022-12-15 13:21:38 +0530

    fourth commit

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -8,7 +8,6 @@
             </div>
         </div>
     </div>
-    <!-- test comment -->
     <main class="main-content  mt-0">
         <section>
             <div class="page-header min-vh-100">
@@ -26,7 +25,7 @@
                                             <input type="hidden" name="token" value="{{ $token }}">
 
                                         <div class="flex flex-col mb-3">
-                                            <input type="email" name="email" class="form-control form-control-lg" placeholder="Email" value="{{ old('email') }}" aria-label="Email">
+                                            <input type="email" name="email" class="form-control form-control-lg" placeholder="Email" readonly value="{{$email}}" aria-label="Email">
                                             @error('email') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                         <div class="flex flex-col mb-3">
@@ -38,7 +37,7 @@
                                             @error('confirm-password') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Send Reset Link</button>
+                                            <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Submit</button>
                                         </div>
                                     </form>
                                 </div>
