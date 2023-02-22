5f3ff7e70bcc117d89da6643f02fbcd9b77d28e5
vaibhavbhasin <vaibhav.bhasin@yahoo.co.in>
2023-02-22 22:41:37 +0530

    right side image for griffin

--- resources/views/auth/Login1.blade.php
+++ resources/views/auth/Login1.blade.php
@@ -65,8 +65,16 @@
                         </div>
                         <div
                             class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                            @php  $array = [
+                                asset('argon/img/grcr_login_right1.jpg'),
+                                asset('argon/img/grcr_login_right2.jpg'),
+                                asset('argon/img/grcr_login_right3.jpg'),
+                            ];
+                            $random_keys=array_rand($array);
+       
+                            @endphp
                             <div class="position-relative  h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                                style="background-image: url({{asset('argon/img/ivana-square.jpg')}});
+                               id="image" style="background-image: url({{$array[$random_keys]}});
               background-size: cover;">
                                 <span class="mask  opacity-6"></span>
                                 <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new
@@ -87,6 +95,7 @@
     <script>
         $(document).ready(function(){
             $('#alert').fadeOut(5000);
+            
         });
     </script>
 @endpush
