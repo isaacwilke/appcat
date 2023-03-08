88224ab65694e191050c1ded931e302378197db4
Vaibhav Bhasin <vaibhav.bhasin@yahoo.co.in>
2023-03-08 22:14:32 +0530

    Update Login1.blade.php

--- resources/views/auth/Login1.blade.php
+++ resources/views/auth/Login1.blade.php
@@ -45,7 +45,7 @@
                                             <label class="form-check-label" for="rememberMe">Remember me</label>
                                         </div>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-lg btn-blue btn-lg w-100 mt-4 mb-0">Sign in</button>
+                                            <button type="submit" id="btnlogin" class="btn btn-lg btn-blue btn-lg w-100 mt-4 mb-0">Sign in</button>
                                         </div>
                                     </form>
                                 </div>
@@ -92,8 +92,8 @@
 
     <script>
 	$( "#griifinloginform" ).submit(function( event ) {
-	  alert( "Handler for .submit() called." );
-	  event.preventDefault();
+	$("#btnlogin").empty();
+	$("#btnlogin").append('<i class="fa fa-refresh fa-spin"></i> Processing');
 	});
 
 
