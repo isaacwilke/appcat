94088cbb15e12fc6e5b9f8a0597b911ab61461eb
Vaibhav Bhasin <vaibhav.bhasin@yahoo.co.in>
2023-03-21 00:08:06 +0530

    Update cancelreservation.blade.php

--- resources/views/mail/cancelreservation.blade.php
+++ resources/views/mail/cancelreservation.blade.php
@@ -1,23 +1,23 @@
 <p>Hello {{$last_name}},</p>
-<br>
+
 <p>We are bummed to see that you have requested a cancellation of your reservation.  We were so looking forward to getting to pamper and care for your cat while you are away. </p>
-<br>
+
 <p>For your reference, here is the link to our <a href="https://griffinrockcatretreat.com/cancellation-policy"_blank">Cancellation Policy</a>.  </p>
-<br>
+
 <p>
 Our staff will review this request and your reservations details and will communicate back to you with the final confirmation of your refund amount. </p> 
-<br>
+
 <p>As a reference your reservation details are below:</p> 
-<br>
+
 <p>Check-in date: {{$reservation['checkin']}}</p> 
 <p>Check-out date: {{$reservation['checkout']}}</p> 
 <p>Number of cats: {{$reservation['no_of_cats']}}</p> 
 <p>Accommodation: [resa_accommodation]</p> 
 <p>Price: {{$reservation['amount_paid']}}</p> 
-<br>
+
 <br>
 <p>If you have any questions in the mean time please contact us directly at <a href="tel:2142335148">214-233-5148</a>.  Our staff will be in touch as soon as they have had time to review this cancellation request.  </p> 
-<br>
+
 <p>Thanks and meow for now. </p> 
-<br>
+
 <p>-Griffin Rock Cat Retreat</p> 
